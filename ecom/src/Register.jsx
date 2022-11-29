40d0c29d5e101d0718c8ed14743de362b9c70bb5
nathan77144 <nathan.gerard@epitech.eu>
2022-11-29 10:51:46 +0100

    css

--- ecom/src/Register.jsx
+++ ecom/src/Register.jsx
@@ -21,12 +21,12 @@ return (
         <form onSubmit={handleSubmit}>
         <h1>Register page</h1>
         <label htmlFor="name">Full name</label>
-        <input value={name} id="name" name="name"  placeholder="Enter your name" ></input>
+        <input value={name}  onChange={(e)=> setName(e.target.value)} id="name" name="name"  placeholder="Enter your name" ></input>
         <label htmlFor="e-mail"> E-mail</label>
         <input value={email} onChange={(e)=> setEmail(e.target.value)} type="email" placeholder="enter your e-mail" id="e-mail" name="email"></input>
         <label htmlFor="password"> Password</label>
         <input  value ={pass} onChange={(e)=> setPass(e.target.value)} type="password" placeholder="*******" id="password" name="password"></input>
-        <button type="submit">Log In</button>
+        <button type="submit">Register</button>
         </form>
         <button onClick={()=>props.onFormSwitch('login')}>Already have an account? Login here.</button>
 
