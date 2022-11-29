4843544e23e4b34a22f4ee81fb2fbfd3a9831cfe
DarkblooM-SR <darkbloompro@gmail.com>
2022-11-29 12:04:10 +0100

    creation page Home

--- ecom/src/App.js
+++ ecom/src/App.js
@@ -2,18 +2,24 @@ import './App.css';
 import React, {useState} from "react"
 import {Login} from "./Login"
 import {Register} from "./Register"
+import {Home} from "./Home"
 
 
 function App() {
-  const[currentForm, setCurrentForm]= useState('login');
-  const toogleForm = (formName) =>{
-    setCurrentForm(formName)
-  }
+  // const[currentForm, setCurrentForm]= useState('login');
+  // const toogleForm = (formName) =>{
+  //   setCurrentForm(formName)
+  // }
+  // return (
+  //   <div className="App">
+  //     {
+  //       currentForm == 'login' ? <Login onFormSwitch={toogleForm}/> :  <Register onFormSwitch={toogleForm}/>
+  //     }
+  //   </div>
+  // );
   return (
-    <div className="App">
-      {
-        currentForm == 'login' ? <Login onFormSwitch={toogleForm}/> :  <Register onFormSwitch={toogleForm}/>
-      }
+    <div className="Home">
+      <Home/>
     </div>
   );
 }
---
+++ ecom/src/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import cart from './img/icon-cart.svg'
+
+export const Home = () => {
+    return (
+        <nav>
+            <div className="left">
+                <a href="#">Products</a>
+                <a href="#">About</a>
+            </div>
+            <div className="mid">
+                {/* Substitution temporaire à remplacer à l'avenir par le logo */}
+                <div className="logo"></div>
+            </div>
+            <div className="right">
+                <img src={cart} width="50"></img>
+                <a href="#">Log in</a>
+                <a href="#">Register</a>
+            </div>
+        </nav>
+    );
+}
