7e983793c601235e9b14b07bad88162975c57484
DarkblooM-SR <darkbloompro@gmail.com>
2022-11-30 16:01:22 +0100

    updated cards

--- ecom/src/Home.jsx
+++ ecom/src/Home.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import cart from './img/icon-cart.svg'
 import logo from './img/logo.png'
 import products from './img/products.png'
+import motherboard from './img/motherboard.png'
 
 export const Home = () => {
     return (
@@ -35,23 +36,61 @@ export const Home = () => {
                         <div className="card-body">
                             <h5 className="card-title">Lorem ipsum</h5>
                             <p className="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil blanditiis reiciendis, illum provident odio obcaecati.</p>
-                            <button className="btn btn-primary">Details</button>
+                            <a href="#">
+                                <button className="btn btn-primary">Details</button>
+                            </a>
                         </div>
                     </div>
                     <div className="card">
                         <img src={products} alt="products" className="card-img-top"/>
                         <div className="card-body">
                             <h5 className="card-title">Lorem ipsum</h5>
-                            <p className="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil blanditiis reiciendis, illum provident odio obcaecati.</p>
-                            <button className="btn btn-primary">Details</button>
+                            <p className="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo pariatur, deleniti minus nesciunt officia quia?</p>
+                            <a href="#">
+                                <button className="btn btn-primary">Details</button>
+                            </a>
                         </div>
                     </div>
                     <div className="card">
                         <img src={products} alt="products" className="card-img-top"/>
                         <div className="card-body">
                             <h5 className="card-title">Lorem ipsum</h5>
-                            <p className="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil blanditiis reiciendis, illum provident odio obcaecati.</p>
-                            <button className="btn btn-primary">Details</button>
+                            <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi velit necessitatibus odio. Recusandae, at placeat!</p>
+                            <a href="#">
+                                <button className="btn btn-primary">Details</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="card">
+                        <img src={motherboard} alt="motherboard" className="card-img-top"/>
+                        <div className="card-body">
+                            <h5 className="card-title">Lorem ipsum</h5>
+                            <p className="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, fuga perspiciatis! Ut doloribus ex nostrum!</p>
+                            <a href="#">
+                                <button className="btn btn-primary">Details</button>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <img src={motherboard} alt="motherboard" className="card-img-top"/>
+                        <div className="card-body">
+                            <h5 className="card-title">Lorem ipsum</h5>
+                            <p className="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab architecto facilis libero error delectus possimus.</p>
+                            <a href="#">
+                                <button className="btn btn-primary">Details</button>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <img src={motherboard} alt="motherboard" className="card-img-top"/>
+                        <div className="card-body">
+                            <h5 className="card-title">Lorem ipsum</h5>
+                            <p className="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero quo odit, voluptatem nihil voluptas recusandae.</p>
+                            <a href="#">
+                                <button className="btn btn-primary">Details</button>
+                            </a>
                         </div>
                     </div>
                 </div>
