c46c499f919f0cf9461edeaa7022eb1b43a4c3f8
marius <marius.mestron@epitech.eu>
2022-12-05 11:50:08 +0100

    [ADD]: panel admin

---
+++ server/routes/web.php
@@ -0,0 +1,21 @@
+<?php
+  
+use Illuminate\Support\Facades\Route;
+  
+use App\Http\Controllers\ProductController;
+  
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group that
+| contains the "web" middleware group. Now create something great!
+|
+*/
+  
+Route::resource('admin/products', ProductController::class);
+// Route::resource('admin/users', UsersController::class);
+
+
