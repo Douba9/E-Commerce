e3bf4c8058813be7deff3f0d91832ad33e8cd7cb
DarkblooM-SR <darkbloompro@gmail.com>
2022-12-12 10:53:12 +0100

    added routes

--- ecom/src/App.js
+++ ecom/src/App.js
@@ -1,13 +1,24 @@
-import './App.css';
+import './App.css'
 import React, { useState } from "react"
 import { Login } from "./Login"
 import { Register } from "./Register"
 import { Home } from "./Home"
 import { Article } from "./Article"
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { render } from '@testing-library/react'
 
 // const cookies = new Cookies();
 
 function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<div className='Home'><Home/></div>}></Route>
+        <Route exact path='/article' element={<div className='Article'><Article/></div>}></Route>
+      </Routes>
+    </Router>
+  )
+
   // const [currentForm, setCurrentForm] = useState('register');
   // const toogleForm = (formName) => {
   //   setCurrentForm(formName)
@@ -20,11 +31,11 @@ function App() {
   //   </div>
   // );
 
-  return (
-    <div className="Home">
-      <Home/>
-    </div>
-  );
+  // return (
+  //   <div className="Home">
+  //     <Home/>
+  //   </div>
+  // );
 
   // return (
   //   <div className="Article">
